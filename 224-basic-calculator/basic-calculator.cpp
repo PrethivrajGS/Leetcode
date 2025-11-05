64f582bac9ef9fb4755a6b412eb6093f4f47fb9a
Prethivraj GS <prithivi2303@gmail.com>
2025-11-05 10:07:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (70.22%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int calculate(string s) {
+         stack<int> st;       
+    int result = 0;       
+    int number = 0;      
+    int sign = 1;         
+
+    for (int i = 0; i < s.size(); i++) {
+        char c = s[i];
+
+        if (isdigit(c)) {
+            number = number * 10 + (c - '0'); // form the number
+        } 
+        else if (c == '+') {
+            result += sign * number;
+            number = 0;
+            sign = 1;
+        } 
+        else if (c == '-') {
+            result += sign * number;
+            number = 0;
+            sign = -1;
+        } 
+        else if (c == '(') {
+           
+            st.push(result);
+            st.push(sign);
+            result = 0;
+            sign = 1;
+        } 
+        else if (c == ')') {
+            result += sign * number;
+            number = 0;
+
+            result *= st.top();
+            st.pop();
+
+            result += st.top();
+            st.pop();
+        }
+    }
+    result += sign * number;
+    return result;
+    }
+};
