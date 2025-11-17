88c08cc329aa24e39dc8aa92e1f1e54df487345f
Prethivraj GS <prithivi2303@gmail.com>
2025-11-17 09:36:34 +0530

    Time: 54 ms (46.38%) | Memory: 79.7 MB (30.50%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<string>> result;
+    vector<string> path;
+
+    bool isPalindrome(const string &s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+            l++; 
+            r--;
+        }
+        return true;
+    }
+
+    void backtrack(int start, const string &s) {
+        if (start == s.size()) {
+            result.push_back(path);
+            return;
+        }
+
+        for (int i = start; i < s.size(); i++) {
+            if (isPalindrome(s, start, i)) {
+                path.push_back(s.substr(start, i - start + 1));
+                backtrack(i + 1, s);
+                path.pop_back();  // backtrack
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        backtrack(0, s);
+        return result;
+    }
+};
