9d2bd41db1234cc681b9e026bf40396d0ee887db
Prethivraj GS <prithivi2303@gmail.com>
2025-12-20 18:53:04 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (88.60%) - LeetSync

---
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    pair<int,int> dfs(TreeNode* root) {
+        if (!root) return {0, 0};
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        // rob this node
+        int rob = root->val + left.second + right.second;
+
+        // do not rob this node
+        int notRob = max(left.first, left.second) +
+                     max(right.first, right.second);
+
+        return {rob, notRob};
+    }
+
+    int rob(TreeNode* root) {
+        auto res = dfs(root);
+        return max(res.first, res.second);
+    }
+};
+
