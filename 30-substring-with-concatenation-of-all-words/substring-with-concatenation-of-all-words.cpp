1d0ebd93f6be8ad2c6c1fd78dcf7629015bc4884
Prethivraj GS <prithivi2303@gmail.com>
2025-12-21 18:36:20 +0530

    Time: 19 ms (82.02%) | Memory: 23.7 MB (94.10%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> result;
+        if (words.empty()) return result;
+
+        int wordLen = words[0].size();
+        int wordCount = words.size();
+        int totalLen = wordLen * wordCount;
+
+        unordered_map<string, int> need;
+        for (auto &w : words) need[w]++;
+
+        // Try each offset
+        for (int i = 0; i < wordLen; i++) {
+            unordered_map<string, int> window;
+            int left = i, count = 0;
+
+            for (int right = i; right + wordLen <= s.size(); right += wordLen) {
+                string word = s.substr(right, wordLen);
+
+                if (need.count(word)) {
+                    window[word]++;
+                    count++;
+
+                    // Too many of this word → shrink
+                    while (window[word] > need[word]) {
+                        string leftWord = s.substr(left, wordLen);
+                        window[leftWord]--;
+                        left += wordLen;
+                        count--;
+                    }
+
+                    // Valid concatenation found
+                    if (count == wordCount) {
+                        result.push_back(left);
+
+                        // Move window forward
+                        string leftWord = s.substr(left, wordLen);
+                        window[leftWord]--;
+                        left += wordLen;
+                        count--;
+                    }
+                } else {
+                    // Reset window
+                    window.clear();
+                    count = 0;
+                    left = right + wordLen;
+                }
+            }
+        }
+
+        return result;
+    }
+};
