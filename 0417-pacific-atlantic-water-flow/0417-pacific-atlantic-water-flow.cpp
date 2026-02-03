67b15451172998a7dcc737ac47583591b4804ec1
Prethivraj GS <prithivi2303@gmail.com>
2026-02-03 19:36:52 +0530

    Time: 11 ms (39.62%), Space: 22.2 MB (79.98%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int m, n;
+    vector<vector<int>> dirs{{1,0},{-1,0},{0,1},{0,-1}};
+
+    void dfs(int r, int c, vector<vector<int>>& heights,
+             vector<vector<bool>>& ocean) {
+        ocean[r][c] = true;
+
+        for (auto& d : dirs) {
+            int nr = r + d[0];
+            int nc = c + d[1];
+
+            if (nr < 0 || nc < 0 || nr >= m || nc >= n) continue;
+            if (ocean[nr][nc]) continue;
+            if (heights[nr][nc] < heights[r][c]) continue;
+
+            dfs(nr, nc, heights, ocean);
+        }
+    }
+
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        m = heights.size();
+        n = heights[0].size();
+
+        vector<vector<bool>> pacific(m, vector<bool>(n, false));
+        vector<vector<bool>> atlantic(m, vector<bool>(n, false));
+
+        for (int c = 0; c < n; c++)
+            dfs(0, c, heights, pacific);
+        for (int r = 0; r < m; r++)
+            dfs(r, 0, heights, pacific);
+
+        for (int c = 0; c < n; c++)
+            dfs(m - 1, c, heights, atlantic);
+        for (int r = 0; r < m; r++)
+            dfs(r, n - 1, heights, atlantic);
+
+        vector<vector<int>> result;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (pacific[r][c] && atlantic[r][c])
+                    result.push_back({r, c});
+            }
+        }
+
+        return result;
+    }
+};
